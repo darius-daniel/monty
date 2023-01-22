6a8a4310417fcd1d60b89c109e59f8e3539e4c70
Darius Daniel <dariusdaniel771@gmail.com>
2023-01-22 01:49:48 +0100

    Added new files and removed other

--- push.c
+++ push.c
@@ -40,18 +40,19 @@ void pushInt(stack_t **stack, uInt line_number)
  * checkFormat - checks the input format to the function
  * @line_number: opcode line number
  * @group: a struct containing a group of global variables
+ * Return: 1 string in var_group can be converted to a num. 0 otherwise
 */
 void checkFormat(GLOBALS *group, uInt line_number)
 {
 	char *str_num;
 	int i = 0, not_digit = 0;
 
-	if (group != NULL)
+	str_num = group->arg;
+	if (str_num != NULL)
 	{
-		str_num = group->arg;
 		while (str_num[i] != '\0')
 		{
-			if (str_num[i] < 48 || str_num[i] > 57)
+			if (str_num[i] < '0' || str_num[i] > '9')
 			{
 				not_digit = 1;
 				break;
@@ -60,7 +61,7 @@ void checkFormat(GLOBALS *group, uInt line_number)
 		}
 	}
 
-	if (group->arg == NULL || not_digit == 1)
+	if (str_num == NULL || not_digit == 1)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		freeGroup(&var_group);
